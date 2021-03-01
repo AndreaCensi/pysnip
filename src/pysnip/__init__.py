67cb8e8b2a7a6314badbc2c0c8244b676d1bb0fa
Andrea Censi <acensi@ethz.ch>
2021-03-01 20:54:14 +0100

    fix

--- src/pysnip/__init__.py
+++ src/pysnip/__init__.py
@@ -3,5 +3,6 @@ __date__ = "2021-03-01T20:49:19.166261"
 from zuper_commons.logs import ZLogger
 
 logger = ZLogger(__name__)
+logger.hello_module(name=__name__, filename=__file__, version=__version__, date=__date__)
 
 from .write_source import *
