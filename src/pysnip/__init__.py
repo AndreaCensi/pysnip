a32a0bb3e04b99c691171ab00137c2135db845e3
Andrea Censi <acensi@ethz.ch>
2021-05-15 22:35:49 +0200

    Bump to version 7.1.2105152035

--- src/pysnip/__init__.py
+++ src/pysnip/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "7.1.2105151126"
-__date__ = "2021-05-15T11:26:52.483221+00:00"
+__version__ = "7.1.2105152035"
+__date__ = "2021-05-15T20:35:49.197687+00:00"
 from zuper_commons.logs import ZLogger
 
 logger = ZLogger(__name__)
