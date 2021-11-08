18c2bc8a3f8eca9963c02fe757edd88a11954448
Andrea Censi <acensi@ethz.ch>
2021-11-08 18:28:20 +0100

    misc

--- src/pysnip/__init__.py
+++ src/pysnip/__init__.py
@@ -1,8 +1,11 @@
 __version__ = "7.2"
-__date__ = "2021-08-11T13:29:21.087789+00:00"
+__date__ = ""
+
 from zuper_commons.logs import ZLogger
 
 logger = ZLogger(__name__)
 logger.hello_module(name=__name__, filename=__file__, version=__version__, date=__date__)
 
 from .write_source import *
+
+logger.hello_module_finished(__name__)
